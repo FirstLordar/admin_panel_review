fe81dd27740653cac0baa18d92bd34b0d50d6e80
FirstLordar <arzhan2517@gmail.com>
2024-04-24 14:35:19 +0300

    Домашнее задание дубль 2

---
+++ middlewares/cors.js
@@ -0,0 +1,14 @@
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+}
+
+// const allowedCors = [
+//     'https://practicum.yandex.ru',
+//     'https://students-projects.ru',
+//     'localhost:3000'
+//   ];
+
+  module.exports = cors
